8bf0cbabf4088d5b4b26aa662e227033cd7b4cbc
gmw15 <gmw6593@gmail.com>
2017-01-16 20:48:34 +0000

    Start on Puzzles
    Gui for first puzzles appears with trigger box

---
+++ ElseIf/Assets/Quizzes/CanvasScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour {
+
+public GameObject menu;
+private bool isShowing = false;
+
+	// Use this for initialization
+	void Start () {
+		menu.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//menu.SetActive(isShowing);
+	}
+
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.name == "FPSController"){
+			isShowing=true;
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.name == "FPSController"){
+			isShowing=false;
+		}
+	}
+
+	void OnGUI(){
+		if(isShowing==true){
+			menu.SetActive(true);
+		}
+		else{
+			menu.SetActive(false);
+		}
+	}
+
+}
