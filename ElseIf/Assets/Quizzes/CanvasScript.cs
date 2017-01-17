260d60f0ad4525959ce8d2c9276c5d91ac5a4f27
gmw15 <gmw6593@gmail.com>
2017-01-17 18:50:14 +0000

    Quiz1_Start
    Getting input text to check agaisnt user input

--- ElseIf/Assets/Quizzes/CanvasScript.cs
+++ ElseIf/Assets/Quizzes/CanvasScript.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class CanvasScript : MonoBehaviour {
 
-public GameObject menu;
-private bool isShowing = false;
+    //MAKE GUI APPEAR
+    public GameObject menu;
+    private bool isShowing = false;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject submitButton;
+    //public GameObject answer1TF;
+    public Text answer1TF;
+    public Text result;
+
+    // Use this for initialization
+    void Start () {
 		menu.SetActive(false);
 	}
 	
@@ -38,5 +46,19 @@ private bool isShowing = false;
 			menu.SetActive(false);
 		}
 	}
+
+    //GUI QUIZ
+    public void ClickSubmitButtonQuiz1()
+    {
+        string answer1 = answer1TF.text;
+        if (answer1.Length < 8)
+        {
+            result.text = "Text needs to be long";
+        }
+        else
+        {
+            result.text = "Perfect";
+        }
+    }
 
 }
---
+++ ElseIf/Assets/Quizzes/Quiz1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Quiz1 : MonoBehaviour {
+
+    public GameObject submitButton;
+    //public GameObject answer1TF;
+    public Text answer1TF;
+    public Text result;
+
+    // Use this for initialization
+    void Start () {
+        result.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void ClickSubmitButtonQuiz1()
+    {
+        string answer1 = answer1TF.text;
+
+        if (answer1.Length < 8)
+        {
+            result.text = "Unuckeeeee";
+        }
+        else
+        {
+            result.text = "Correct";
+        }
+    }
+}
