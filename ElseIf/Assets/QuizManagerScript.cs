61854374d62020548deb07ac7d4c1bda49cb54d5
gmw15 <gmw6593@gmail.com>
2017-01-20 16:43:22 +0000

    QuizUpdate_TrueOrFalse

---
+++ ElseIf/Assets/QuizManagerScript.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class QuizManagerScript : MonoBehaviour {
+
+    public Question[] questions;
+    private static List<Question> unansweredQuestions;
+
+    private Question currentQuestion;
+
+    [SerializeField]
+    private Text factText;
+
+    [SerializeField]
+    private Text falseAnswerText;
+
+    [SerializeField]
+    private Text trueAnswerText;
+
+
+    [SerializeField]
+    private float timeBetweenQuestions = 1f;
+
+    [SerializeField]
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+		if (unansweredQuestions == null || unansweredQuestions.Count == 0)
+        {
+            unansweredQuestions = questions.ToList<Question>();
+        }
+        SetCurrentQuestion();
+        Debug.Log(currentQuestion.fact + " is "+ currentQuestion.isTrue);
+	}
+
+    void SetCurrentQuestion()
+    {
+        int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
+        currentQuestion = unansweredQuestions [randomQuestionIndex];
+
+        factText.text = currentQuestion.fact;
+
+        if (currentQuestion.isTrue)
+        {
+            trueAnswerText.text = "Correct";
+            falseAnswerText.text = "Wrong";
+        }
+        else
+        {
+            trueAnswerText.text = "Wrong";
+            falseAnswerText.text = "Correct";
+        }
+    }
+
+    IEnumerator TransitionToNextQuestion()
+    {
+        unansweredQuestions.Remove(currentQuestion);
+        yield return new WaitForSeconds(timeBetweenQuestions);
+
+        //Change to go to next index in the array!!!!!
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void UserSelectsTrue()
+    {
+        animator.SetTrigger("true");
+        if (currentQuestion.isTrue)
+        {
+            Debug.Log("Correct!");
+        }
+        else
+        {
+            Debug.Log("Wrong!");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+    public void UserSelectsFalse()
+    {
+        animator.SetTrigger("false");
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("Correct!");
+        }
+        else
+        {
+            Debug.Log("Wrong!");
+        }
+
+        StartCoroutine(TransitionToNextQuestion());
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ ElseIf/Assets/TriggerWidget.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TriggerWidget : MonoBehaviour {
+
+    public GameObject menuQuiz2;
+    private bool isShowing = false;
+
+    public GameObject trueButton;
+    public GameObject falseButton;
+    //public GameObject answer1TF;
+    public Text questionText;
+
+    // Use this for initialization
+    void Start()
+    {
+        menuQuiz2.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //menu.SetActive(isShowing);
+    }
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "FPSController")
+        {
+            isShowing = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.name == "FPSController")
+        {
+            isShowing = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (isShowing == true)
+        {
+            menuQuiz2.SetActive(true);
+        }
+        else
+        {
+            menuQuiz2.SetActive(false);
+        }
+    }
+}
