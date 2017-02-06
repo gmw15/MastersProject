b2a5c36703fb39efb024c816935b11180e8c43f6
gmw15 <gmw6593@gmail.com>
2017-02-06 20:57:06 +0000

    AI_NavMesh

---
+++ ElseIf/Assets/Enemy_AI/AI.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class AI : MonoBehaviour
+    {
+
+        public NavMeshAgent agent;
+        public ThirdPersonCharacter character;
+
+        public enum State
+        {
+            PATROL,
+            CHASE
+        }
+
+        public State state;
+        private bool alive;
+
+        //Patrolling variables
+        public GameObject[] waypoints;
+        private int waypointInd;
+        public float patrolSpeed = 0.5f;
+
+        //Variables for chasing
+        public float chaseSpeed = 1f;
+        public GameObject target;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+            agent.updatePosition = true;
+            agent.updateRotation = false;
+
+            waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+            waypointInd = Random.Range(0, waypoints.Length);
+
+            state = AI.State.PATROL;
+            alive = true;
+            //Start Finite State Machine
+            StartCoroutine("FSM");
+        }
+
+        IEnumerator FSM()
+        {
+            while (alive)
+            {
+                switch (state)
+                {
+                    case State.PATROL:
+                        Patrol();
+                        break;
+                    case State.CHASE:
+                        Chase();
+                        break;
+                }
+                yield return null;
+            }
+        }
+
+        void Patrol()
+        {
+            agent.speed = patrolSpeed;
+            //if im to far away from the waypoint
+            if (Vector3.Distance(this.transform.position, waypoints[waypointInd].transform.position) >= 2)
+            {
+                agent.SetDestination(waypoints[waypointInd].transform.position);
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            //if im to close to the waypoint
+            else if (Vector3.Distance(this.transform.position, waypoints[waypointInd].transform.position) < 2)
+            {
+                //waypointInd += 1;
+                //if (waypointInd >= waypoints.Length)
+                //{
+                //   waypointInd = 0;
+                //}
+                waypointInd = Random.Range(0, waypoints.Length);
+            }
+            else
+            {
+                //play idle animation if something goes wrong
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+
+        void Chase()
+        {
+            agent.speed = chaseSpeed;
+            agent.SetDestination(target.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+            {
+                state = AI.State.CHASE;
+                target = coll.gameObject;
+            }
+        }
+    }
+}
---
+++ ElseIf/Assets/Enemy_AI/enemySight.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    public class enemySight : MonoBehaviour
+    {
+
+        public NavMeshAgent agent;
+        public ThirdPersonCharacter character;
+
+        public enum State
+        {
+            PATROL,
+            CHASE,
+            INVESTIGATE
+        }
+
+        public State state;
+        private bool alive;
+
+        //Patrolling variables
+        public GameObject[] waypoints;
+        private int waypointInd;
+        public float patrolSpeed = 0.5f;
+
+        //Variables for chasing
+        public float chaseSpeed = 1f;
+        public GameObject target;
+
+        //Variables for investigating
+        private Vector3 investigateSpot;
+        private float timer = 0;
+        public float investigateWait = 10;
+
+        //Variables for sight
+        public float heightMultiplier;
+        public float sightDist = 10;
+
+        // Use this for initialization
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+            agent.updatePosition = true;
+            agent.updateRotation = false;
+
+            waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+            waypointInd = Random.Range(0, waypoints.Length);
+
+            state = enemySight.State.PATROL;
+            alive = true;
+
+            heightMultiplier = 1.36f;
+
+            //Start Finite State Machine
+            StartCoroutine("FSM");
+        }
+
+        IEnumerator FSM()
+        {
+            while (alive)
+            {
+                switch (state)
+                {
+                    case State.PATROL:
+                        Patrol();
+                        break;
+                    case State.CHASE:
+                        Chase();
+                        break;
+                    case State.INVESTIGATE:
+                        Investigate();
+                        break;
+                }
+                yield return null;
+            }
+        }
+
+        void Patrol()
+        {
+            agent.speed = patrolSpeed;
+            //if im to far away from the waypoint
+            if (Vector3.Distance(this.transform.position, waypoints[waypointInd].transform.position) >= 2)
+            {
+                agent.SetDestination(waypoints[waypointInd].transform.position);
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            //if im to close to the waypoint
+            else if (Vector3.Distance(this.transform.position, waypoints[waypointInd].transform.position) < 2)
+            {
+                //waypointInd += 1;
+                //if (waypointInd >= waypoints.Length)
+                //{
+                //   waypointInd = 0;
+                //}
+                waypointInd = Random.Range(0, waypoints.Length);
+            }
+            else
+            {
+                //play idle animation if something goes wrong
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+
+        void Chase()
+        {
+            agent.speed = chaseSpeed;
+            agent.SetDestination(target.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+        }
+
+        void Investigate()
+        {
+            timer += Time.deltaTime;
+            RaycastHit hit;
+            Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, transform.forward * sightDist, Color.green);
+            Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized * sightDist, Color.green);
+            Debug.DrawRay(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized * sightDist, Color.green);
+
+            if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, transform.forward, out hit, sightDist))
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    state = enemySight.State.CHASE;
+                    target = hit.collider.gameObject;
+                }
+            }
+            if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward + transform.right).normalized, out hit, sightDist))
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    state = enemySight.State.CHASE;
+                    target = hit.collider.gameObject;
+                }
+            }
+            if (Physics.Raycast(transform.position + Vector3.up * heightMultiplier, (transform.forward - transform.right).normalized, out hit, sightDist))
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    state = enemySight.State.CHASE;
+                    target = hit.collider.gameObject;
+                }
+            }
+            //stop player moving to investigate
+            agent.SetDestination(this.transform.position);
+            character.Move(Vector3.zero, false, false);
+            transform.LookAt(investigateSpot);
+            if (timer >= investigateWait)
+            {
+                state = enemySight.State.PATROL;
+                timer = 0;
+            }
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.tag == "Player")
+            {
+                state = enemySight.State.INVESTIGATE;
+                investigateSpot = coll.gameObject.transform.position;
+            }
+        }
+    }
+}
